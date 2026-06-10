5d30cec6236c1e84b5bd3043d9c88157f76b1666
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-06-11 01:40:03 +0800

    Clear old localStorage

---
+++ assets/js/common/_time.js
@@ -0,0 +1,11 @@
+import * as params from "@params";
+
+(() => {
+  const currBuildTime = params.buildTime;
+  const prevBuildTime = localStorage.getItem("buildTime") || 0;
+
+  if (prevBuildTime < currBuildTime) {
+    localStorage.clear();
+    localStorage.setItem("buildTime", currBuildTime);
+  }
+})();
--- assets/js/common/main.js
+++ assets/js/common/main.js
@@ -1,2 +1,3 @@
+import "./_time.js";
 import "./_menu.js";
 import "./_theme.js";
